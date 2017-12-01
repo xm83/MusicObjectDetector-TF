19d499961f5e4d2d7d72a22c0965e4a20bafac8c
Alexander Pacha <alexander.pacha@gmail.com>
2017-12-01 13:42:01 +0100

    Split TrainModel.ps1 into two files: One for preparing the datasets and one for actual training

--- MusicObjectDetector/TrainModel.ps1
+++ MusicObjectDetector/TrainModel.ps1
@@ -16,18 +16,6 @@ cd $pathToSourceRoot
 echo "Appending research folder $($pathToGitRoot)research to temporary PYTHONPATH"
 $env:PYTHONPATH = $env:PYTHONPATH;"$($pathToSourceRoot)research"
 
-# echo "Testing correct setup"
-# cd ../research
-# python object_detection/builders/model_builder_test.py
-# 
-# echo "Generating data-record in Tensorflow-format"
-# cd ../MusicObjectDetector
-# python muscima_image_cutter.py
-# python DatasetSplitter.py
-# python create_muscima_tf_record.py --data_dir=data --set=training --annotations_dir=Annotations --output_path=training.record --label_map_path=mapping.txt
-# python create_muscima_tf_record.py --data_dir=data --set=validation --annotations_dir=Annotations --output_path=validation.record --label_map_path=mapping.txt
-# python create_muscima_tf_record.py --data_dir=data --set=test --annotations_dir=Annotations --output_path=test.record --label_map_path=mapping.txt
-
 $pathToPipelineConfig = "$($pathToSourceRoot)faster_rcnn_resnet50_muscima_windows.config"
 $trainingDirectory = $pathToSourceRoot
 cd ../research
