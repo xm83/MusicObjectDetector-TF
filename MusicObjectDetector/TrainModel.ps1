f24016f582d205a1ad342c1699060c2a63051c39
Alexander Pacha <alexander.pacha@gmail.com>
2017-12-01 12:12:02 +0100

    Updated configuration and adding training script

--- MusicObjectDetector/TrainModel.ps1
+++ MusicObjectDetector/TrainModel.ps1
@@ -1,5 +1,5 @@
-$pathToGitRoot = "C:\Users\Alex\Repositories\MusicObjectDetector-TF\"
-$pathToSourceRoot = "C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\"
+$pathToGitRoot = "C:/Users/Alex/Repositories/MusicObjectDetector-TF/"
+$pathToSourceRoot = "C:/Users/Alex/Repositories/MusicObjectDetector-TF/MusicObjectDetector/"
 $pathToTranscript = "$($pathToSourceRoot)"
 
 # Allowing wider outputs https://stackoverflow.com/questions/7158142/prevent-powergui-from-truncating-the-output
@@ -13,27 +13,28 @@ $pswindow.buffersize = $newsize
 Start-Transcript -path "$($pathToTranscript)Transcript.txt" -append
 
 cd $pathToSourceRoot
-echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
-$env:PYTHONPATH = $pathToSourceRoot
 echo "Appending research folder $($pathToGitRoot)research to temporary PYTHONPATH"
-$env:PYTHONPATH = "$($pathToSourceRoot)research"
-echo "Appending slim folder $($pathToGitRoot)research\slim to temporary PYTHONPATH"
-$env:PYTHONPATH = "$($pathToSourceRoot)research\slim"
+$env:PYTHONPATH = $env:PYTHONPATH;"$($pathToSourceRoot)research"
 
-#echo "Testing correct setup"
-#cd ..\research
-#python object_detection/builders/model_builder_test.py
-#
-#echo "Generating data-record in Tensorflow-format"
-#cd ..\MusicObjectDetector
-#python muscima_image_cutter.py
-#python DatasetSplitter.py
-#python create_muscima_tf_record.py --data_dir=data --set=training --annotations_dir=Annotations --output_path=training.record --label_map_path=mapping.txt
-#python create_muscima_tf_record.py --data_dir=data --set=validation --annotations_dir=Annotations --output_path=validation.record --label_map_path=mapping.txt
-#python create_muscima_tf_record.py --data_dir=data --set=test --annotations_dir=Annotations --output_path=test.record --label_map_path=mapping.txt
+# echo "Testing correct setup"
+# cd ../research
+# python object_detection/builders/model_builder_test.py
+# 
+# echo "Generating data-record in Tensorflow-format"
+# cd ../MusicObjectDetector
+# python muscima_image_cutter.py
+# python DatasetSplitter.py
+# python create_muscima_tf_record.py --data_dir=data --set=training --annotations_dir=Annotations --output_path=training.record --label_map_path=mapping.txt
+# python create_muscima_tf_record.py --data_dir=data --set=validation --annotations_dir=Annotations --output_path=validation.record --label_map_path=mapping.txt
+# python create_muscima_tf_record.py --data_dir=data --set=test --annotations_dir=Annotations --output_path=test.record --label_map_path=mapping.txt
 
-$pathToPipelineConfig = "$($pathToSourceRoot)faster_rcnn_resnet50_muscima.config"
+$pathToPipelineConfig = "$($pathToSourceRoot)faster_rcnn_resnet50_muscima_windows.config"
 $trainingDirectory = $pathToSourceRoot
-python ..\research\object_detection/train.py --logtostderr --pipeline_config_path=$pathToPipelineConfig --train_dir=$trainingDirectory
+cd ../research
+#python object_detection/train.py --logtostderr --pipeline_config_path=$pathToPipelineConfig --train_dir=$trainingDirectory
+python object_detection/train.py --logtostderr --pipeline_config_path=C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\faster_rcnn_resnet50_muscima_windows.config --train_dir=C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\data\training-checkpoints
+
+python object_detection/eval.py --logtostderr --pipeline_config_path=C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\faster_rcnn_resnet50_muscima_windows.config --checkpoint_dir=C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\data --eval_dir=C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\data\validation-checkpoints
+
 
 Stop-Transcript
