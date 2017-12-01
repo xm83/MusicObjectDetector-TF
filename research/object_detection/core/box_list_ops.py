3fae8fc824cb6a8c73bcea2d26a1496a3e4403fd
Kwon-Young Choi <kwon-young.choi@irisa.fr>
2017-12-01 10:45:13 +0100

    changed bbox range limit to 1.2

--- research/object_detection/core/box_list_ops.py
+++ research/object_detection/core/box_list_ops.py
@@ -777,7 +777,7 @@ def to_absolute_coordinates(boxlist,
                             height,
                             width,
                             check_range=True,
-                            maximum_normalized_coordinate=1.01,
+                            maximum_normalized_coordinate=1.2,
                             scope=None):
   """Converts normalized box coordinates to absolute pixel coordinates.
 
