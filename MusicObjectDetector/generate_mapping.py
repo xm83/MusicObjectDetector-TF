169c9091ff0a0ea8313355274b8dc22c4d76b395
Kwon-Young Choi <kwon-young.choi@irisa.fr>
2017-12-05 23:24:55 +0100

    generate mapping by filtering out classes with less than 50 samples, with exceptions

--- MusicObjectDetector/generate_mapping.py
+++ MusicObjectDetector/generate_mapping.py
@@ -3,6 +3,7 @@ import muscima.io
 import glob
 from collections import Counter
 import argparse
+import pandas
 from tqdm import tqdm
 
 
@@ -23,10 +24,21 @@ for value in data.values():
         datas.append(val)
 
 c = Counter([x.clsname for x in datas])
+# keys = [key for key, _ in c.most_common()]
+# df = pandas.DataFrame.from_dict(c, orient='index')
+# ax = df.ix[keys].plot(kind='bar', figsize=(20, 20))
+# ax.get_figure().savefig("class_histogram.png")
+
+exception = ["double_sharp",
+             "numeral_2",
+             "numeral_5",
+             "numeral_6",
+             "numeral_7",
+             "numeral_8"]
 
 filtered_class_id = []
 for key, value in c.items():
-    if value >= args.nbr_example_limit:
+    if value >= args.nbr_example_limit or key in exception:
         filtered_class_id.append(key)
 filtered_class_id.sort()
 with open(args.mapping_filepath, "w") as f:
