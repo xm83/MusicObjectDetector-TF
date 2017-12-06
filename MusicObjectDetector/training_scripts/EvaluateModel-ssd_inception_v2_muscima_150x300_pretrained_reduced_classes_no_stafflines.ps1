cfe11a032e62a4eb9eff6b1b67c778553f7810a2
Alexander Pacha <alexander.pacha@gmail.com>
2017-12-06 23:25:09 +0100

    Adding configuration and train/eval scripts for completed training

---
+++ MusicObjectDetector/training_scripts/EvaluateModel-ssd_inception_v2_muscima_150x300_pretrained_reduced_classes_no_stafflines.ps1
@@ -0,0 +1,11 @@
+$pathToGitRoot = "C:/Users/alpa/Repositories/MusicObjectDetector-TF"
+$pathToSourceRoot = "$($pathToGitRoot)/MusicObjectDetector"
+$pathToTranscript = "$($pathToSourceRoot)/Transcripts"
+$configuration = "ssd_inception_v2_muscima_150x300_pretrained_reduced_classes_no_stafflines"
+
+cd $pathToGitRoot/research
+
+Start-Transcript -path "$($pathToTranscript)/EvaluateModel-$($configuration).txt" -append
+echo "Validate with $($configuration) configuration"
+python object_detection/eval.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --checkpoint_dir="$($pathToSourceRoot)/data/training-checkpoints-$($configuration)" --eval_dir="$($pathToSourceRoot)/data/validation-checkpoints-$($configuration)"
+Stop-Transcript
