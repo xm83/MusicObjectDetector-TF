0607004787552058f44e1c3eb034728b8cfb2286
Alexander Pacha <alexander.pacha@gmail.com>
2017-12-07 23:33:02 +0100

    Adding configuration and train/eval scripts for currently running configuration R-FCN with Inception ResNet v2

---
+++ MusicObjectDetector/training_scripts/TrainModel-rfcn_inception_resnet_v2_atrous_muscima_pretrained.ps1
@@ -0,0 +1,11 @@
+$pathToGitRoot = "C:/Users/alpa/Repositories/MusicObjectDetector-TF"
+$pathToSourceRoot = "$($pathToGitRoot)/MusicObjectDetector"
+$pathToTranscript = "$($pathToSourceRoot)/Transcripts"
+$configuration = "rfcn_inception_resnet_v2_atrous_muscima_pretrained"
+
+cd $pathToGitRoot/research
+
+Start-Transcript -path "$($pathToTranscript)/TrainModel-$($configuration).txt" -append
+echo "Training with $($configuration) configuration"
+python object_detection/train.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --train_dir="$($pathToSourceRoot)/data/training-checkpoints-$($configuration)"
+Stop-Transcript
