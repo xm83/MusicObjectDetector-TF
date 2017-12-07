dd617756681ccec49240737a4b40719cc574fce2
Alexander Pacha <alexander.pacha@gmail.com>
2017-12-07 14:53:03 +0100

    Adding currently training configuration and train/eval scripts

---
+++ MusicObjectDetector/training_scripts/TrainModel-faster_rcnn_inception_resnet_v2_atrous_muscima_pretrained_no_staff_lines.ps1
@@ -0,0 +1,11 @@
+$pathToGitRoot = "C:/Users/Alex/Repositories/MusicObjectDetector-TF"
+$pathToSourceRoot = "$($pathToGitRoot)/MusicObjectDetector"
+$pathToTranscript = "$($pathToSourceRoot)/Transcripts"
+$configuration = "faster_rcnn_inception_resnet_v2_atrous_muscima_pretrained_no_staff_lines"
+
+cd $pathToGitRoot/research
+
+Start-Transcript -path "$($pathToTranscript)/TrainModel-$($configuration).txt" -append
+echo "Training with $($configuration) configuration"
+python object_detection/train.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --train_dir="$($pathToSourceRoot)/data/training-checkpoints-$($configuration)"
+Stop-Transcript
