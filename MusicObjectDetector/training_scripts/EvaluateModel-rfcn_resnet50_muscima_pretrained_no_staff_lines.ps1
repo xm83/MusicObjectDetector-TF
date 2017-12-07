fe782d999507d1cf31fe9186d087dbb4199e0698
Alexander Pacha <alexander.pacha@gmail.com>
2017-12-07 15:52:08 +0100

    Adding configuration and train/eval scripts for upcoming training on Monsti with RFCN + pretraining.

---
+++ MusicObjectDetector/training_scripts/EvaluateModel-rfcn_resnet50_muscima_pretrained_no_staff_lines.ps1
@@ -0,0 +1,11 @@
+$pathToGitRoot = "C:/Users/Alex/Repositories/MusicObjectDetector-TF"
+$pathToSourceRoot = "$($pathToGitRoot)/MusicObjectDetector"
+$pathToTranscript = "$($pathToSourceRoot)/Transcripts"
+$configuration = "rfcn_resnet50_muscima_pretrained_no_staff_lines"
+
+cd $pathToGitRoot/research
+
+Start-Transcript -path "$($pathToTranscript)/EvaluateModel-$($configuration).txt" -append
+echo "Validate with $($configuration) configuration"
+python object_detection/eval.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --checkpoint_dir="$($pathToSourceRoot)/data/training-checkpoints-$($configuration)" --eval_dir="$($pathToSourceRoot)/data/validation-checkpoints-$($configuration)"
+Stop-Transcript
