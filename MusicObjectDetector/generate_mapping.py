164c34fb07a75afc9558accf1f65bb3163ef1c4b
Kwon-Young Choi <kwon-young.choi@irisa.fr>
2017-12-10 23:12:01 +0100

    configuration file for various experiments

--- MusicObjectDetector/generate_mapping.py
+++ MusicObjectDetector/generate_mapping.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("dataset_dir")
 parser.add_argument("mapping_filepath")
 parser.add_argument("--nbr_example_limit", type=int, default=50)
+parser.add_argument("--remove_line_shaped_or_construct", type=bool, default=False)
 args = parser.parse_args()
 
 names = glob.glob(os.path.join(args.dataset_dir, "*.xml"))
@@ -24,6 +25,7 @@ for value in data.values():
         datas.append(val)
 
 c = Counter([x.clsname for x in datas])
+__import__('pdb').set_trace()
 # keys = [key for key, _ in c.most_common()]
 # df = pandas.DataFrame.from_dict(c, orient='index')
 # ax = df.ix[keys].plot(kind='bar', figsize=(20, 20))
@@ -35,11 +37,36 @@ exception = ["double_sharp",
              "numeral_6",
              "numeral_7",
              "numeral_8"]
-
+line_shaped_or_construct = ["stem",
+                            "beam",
+                            "thin_barline",
+                            "measure_separator",
+                            "slur",
+                            "tie",
+                            "key_signature",
+                            "dynamics_text",
+                            "hairpin-decr.",
+                            "other_text",
+                            "tuple",
+                            "hairpin-cresc.",
+                            "time_signature",
+                            "staff_grouping",
+                            "trill",
+                            "tenuto",
+                            "tempo_text",
+                            "multi-staff_brace",
+                            "multiple-note_tremolo",
+                            "repeat",
+                            "multi-staff_bracket",
+                            "tuple_bracket/line"]
 filtered_class_id = []
 for key, value in c.items():
     if value >= args.nbr_example_limit or key in exception:
-        filtered_class_id.append(key)
+        if args.remove_line_shaped_or_construct:
+            if key not in line_shaped_or_construct:
+                filtered_class_id.append(key)
+        else:
+            filtered_class_id.append(key)
 filtered_class_id.sort()
 with open(args.mapping_filepath, "w") as f:
     for i, classname in enumerate(filtered_class_id):
