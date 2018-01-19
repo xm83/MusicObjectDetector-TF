3c48206f7f3651790c6555fb433a5bc4f766a03a
Alexander Pacha <alexander.pacha@gmail.com>
2018-01-19 16:35:10 +0100

    Simplified dimension clustering generation by using pandas and unified names.

--- MusicObjectDetector/muscima_dimension_clustering.py
+++ MusicObjectDetector/muscima_dimension_clustering.py
@@ -11,17 +11,23 @@ from omrdatasettools.image_generators.MuscimaPlusPlusImageGenerator import Musci
 from tqdm import tqdm
 
 
-def collect_dimensions(muscima_image_directory: str,
-                       muscima_pp_raw_data_directory: str,
-                       exported_absolute_dimensions_file_path: str,
-                       exported_relative_dimensions_file_path: str):
+def create_statistics_for_full_images(muscima_image_directory: str,
+                                      muscima_pp_raw_data_directory: str,
+                                      exported_absolute_dimensions_file_path: str,
+                                      exported_relative_dimensions_file_path: str):
     image_paths = glob(muscima_image_directory)
+
+    if os.path.exists(exported_absolute_dimensions_file_path):
+        os.remove(exported_absolute_dimensions_file_path)
+
+    if os.path.exists(exported_relative_dimensions_file_path):
+        os.remove(exported_relative_dimensions_file_path)
 
     image_generator = MuscimaPlusPlusImageGenerator()
     all_xml_files = image_generator.get_all_xml_file_paths(muscima_pp_raw_data_directory)
 
-    all_objects: List[Tuple[int, int, List[CropObject]]] = []
-
+    absolute_dimensions = []
+    relative_dimensions = []
     for xml_file in tqdm(all_xml_files, desc='Parsing annotation files'):
         crop_objects = image_generator.load_crop_objects_from_xml_file(xml_file)
 
@@ -37,56 +43,35 @@ def collect_dimensions(muscima_image_directory: str,
             if image_path_writer == writer and image_path_page == page:
                 path = image_path
                 break
-
+        
         image = Image.open(path, "r")  # type: Image.Image
-        width = image.width
-        height = image.height
+        image_width = image.width
+        image_height = image.height
 
-        all_objects.append((width, height, crop_objects))
+        for crop_object in crop_objects:
+            class_name = crop_object.clsname
+            top, left, bottom, right = crop_object.bounding_box
+            width = right - left
+            height = bottom - top
+            x_center = width / 2.0 + left
+            y_center = height / 2.0 + top
 
-    if os.path.exists(exported_absolute_dimensions_file_path):
-        os.remove(exported_absolute_dimensions_file_path)
+            absolute_dimensions.append([class_name, left, right, top, bottom, x_center, y_center, width, height])
+            relative_dimensions.append([class_name, left / image_width, right / image_width,
+                                        top / image_height, bottom / image_height,
+                                        x_center / image_width, y_center / image_height,
+                                        width / image_width, height / image_height])
 
-    if os.path.exists(exported_relative_dimensions_file_path):
-        os.remove(exported_relative_dimensions_file_path)
-
-    create_statistics_in_csv_format(exported_absolute_dimensions_file_path, exported_relative_dimensions_file_path,
-                                    all_objects)
-
-
-def create_statistics_in_csv_format(exported_absolute_dimensions_file_path: str,
-                                    exported_relative_dimensions_file_path: str,
-                                    all_objects: List[Tuple[int, int, List[CropObject]]]):
-    with open(exported_absolute_dimensions_file_path, "a") as absolute_dimensions_file:
-        with open(exported_relative_dimensions_file_path, "a") as relative_dimensions_file:
-            absolute_dimensions_file.write("class,xmin,xmax,ymin,ymax,x_c,y_c,width,height\n")
-            relative_dimensions_file.write("class,xmin,xmax,ymin,ymax,x_c,y_c,width,height\n")
-
-            for (image_width, image_height, crop_objects) in all_objects:
-                for crop_object in crop_objects:
-                    class_name = crop_object.clsname
-                    top, left, bottom, right = crop_object.bounding_box
-                    width = right - left
-                    height = bottom - top
-                    x_center = width / 2.0 + left
-                    y_center = height / 2.0 + top
-
-                    absolute_dimensions_file.write(
-                        "{0},{1},{2},{3},{4},{5},{6},{7},{8}\n".format(class_name, left, right, top, bottom, x_center,
-                                                                       y_center, width, height))
-                    relative_dimensions_file.write(
-                        "{0},{1},{2},{3},{4},{5},{6},{7},{8}\n".
-                            format(class_name, left / image_width, right / image_width,
-                                   top / image_height, bottom / image_height,
-                                   x_center / image_width, y_center / image_height,
-                                   width / image_width, height / image_height)
-                    )
+    absolute_statistics = pandas.DataFrame(absolute_dimensions, columns=["class","xmin","xmax","ymin","ymax","x_c","y_c","width","height"])
+    absolute_statistics.to_csv(exported_absolute_dimensions_file_path, float_format="%.5f", index=False)
+    relative_statistics = pandas.DataFrame(relative_dimensions, columns=["class","xmin","xmax","ymin","ymax","x_c","y_c","width","height"])
+    relative_statistics.to_csv(exported_relative_dimensions_file_path, float_format="%.5f", index=False)
 
 
-def create_cropped_statistics(annotations_csv,
-                              path_to_cropped_images,
-                              exported_absolute_dimensions_file_path,
-                              exported_relative_dimensions_file_path):
+def create_statistics_for_cropped_images(annotations_csv,
+                                         path_to_cropped_images,
+                                         exported_absolute_dimensions_file_path,
+                                         exported_relative_dimensions_file_path):
     annotations = pandas.read_csv(annotations_csv)
     cropped_image_dimensions = []
     for filename in tqdm(os.listdir(path_to_cropped_images)):
@@ -101,7 +86,7 @@ def create_cropped_statistics(annotations_csv,
     absolute_annotations = pandas.concat(
         [annotations[['filename', 'class', 'left', 'right', 'top', 'bottom']], x_center, y_center, width, height],
         axis=1)  # type: pandas.DataFrame
-    absolute_annotations.to_csv(exported_absolute_dimensions_file_path)
+    absolute_annotations.to_csv(exported_absolute_dimensions_file_path, float_format="%.5f", index=False)
 
     # Compute relative annotations
     image_dimensions = pandas.DataFrame(cropped_image_dimensions, columns=['filename', 'image_width', 'image_height'])
@@ -118,7 +103,7 @@ def create_cropped_statistics(annotations_csv,
     relative_annotations = pandas.concat(
         [joined[['filename', 'class']], left, right, top, bottom, x_center, y_center, width, height],
         axis=1)  # type: pandas.DataFrame
-    relative_annotations.to_csv(exported_relative_dimensions_file_path, float_format="%.5f")
+    relative_annotations.to_csv(exported_relative_dimensions_file_path, float_format="%.5f", index=False)
 
 
 if __name__ == "__main__":
@@ -126,10 +111,11 @@ if __name__ == "__main__":
     muscima_pp_raw_dataset_directory = os.path.join(dataset_directory, "muscima_pp_raw")
     muscima_image_directory = os.path.join(dataset_directory, "cvcmuscima_staff_removal")
 
-    collect_dimensions("data/cvcmuscima_staff_removal/*/ideal/*/image/*.png", "data/muscima_pp_raw",
-                       "data/bounding_box_dimensions_absolute.csv", "data/bounding_box_dimensions_relative.csv")
+    create_statistics_for_full_images("data/cvcmuscima_staff_removal/*/ideal/*/image/*.png", "data/muscima_pp_raw",
+                                      "data/bounding_box_dimensions_absolute.csv",
+                                      "data/bounding_box_dimensions_relative.csv")
 
-    create_cropped_statistics(annotations_csv="data/Annotations.csv",
-                              path_to_cropped_images="data/muscima_pp_cropped_images_with_stafflines",
-                              exported_absolute_dimensions_file_path="data/bounding_box_dimensions_cropped_images_absolute.csv",
-                              exported_relative_dimensions_file_path="data/bounding_box_dimensions_cropped_images_relative.csv")
+    create_statistics_for_cropped_images(annotations_csv="data/Annotations.csv",
+                                         path_to_cropped_images="data/muscima_pp_cropped_images_with_stafflines",
+                                         exported_absolute_dimensions_file_path="data/bounding_box_dimensions_cropped_images_absolute.csv",
+                                         exported_relative_dimensions_file_path="data/bounding_box_dimensions_cropped_images_relative.csv")
