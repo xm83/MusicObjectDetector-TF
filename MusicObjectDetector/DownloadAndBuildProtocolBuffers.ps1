c8a924f0a7a9d6a7e816781a2f4bec0e93bcda5b
Alexander Pacha <alexander.pacha@gmail.com>
2018-04-22 22:24:28 +0200

    Fixed DownloadAndBuildProtocolBuffers ps-script

--- MusicObjectDetector/DownloadAndBuildProtocolBuffers.ps1
+++ MusicObjectDetector/DownloadAndBuildProtocolBuffers.ps1
@@ -1,14 +1,6 @@
 $pathToSourceRoot = "C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\"
 $pathToTranscript = "$($pathToSourceRoot)"
 
-# Allowing wider outputs https://stackoverflow.com/questions/7158142/prevent-powergui-from-truncating-the-output
-$pshost = get-host
-$pswindow = $pshost.ui.rawui
-$newsize = $pswindow.buffersize
-$newsize.height = 9999
-$newsize.width = 1500
-$pswindow.buffersize = $newsize
-
 cd $pathToSourceRoot
 echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
 $env:PYTHONPATH = $pathToSourceRoot
@@ -24,11 +16,9 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-# $url = "https://github.com/google/protobuf/releases/download/v2.6.0/protoc-2.6.0-win32.zip"
-# $output = $pathToSourceRoot + "protoc-2.6.0-win32.zip"
-
 $url = "https://github.com/google/protobuf/releases/download/v3.4.0/protoc-3.4.0-win32.zip"
 $output = $pathToSourceRoot + "protoc-3.4.0-win32.zip"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $url -OutFile $output
 
 $protoc_folder = $pathToSourceRoot + "Protoc"
@@ -37,7 +27,13 @@ Unzip $output $protoc_folder
 .\Protoc\bin\protoc.exe --version
 
 cd ..\research
+..\MusicObjectDetector\Protoc\bin\protoc.exe object_detection/protos/*.proto --python_out=.
+cd slim
+python setup.py install
+cd ..
+python setup.py install
 
-..\MusicObjectDetector\Protoc\bin\protoc.exe object_detection\protos\*.proto               --python_out=.
+rm ..\MusicObjectDetector\Protoc -Recurse
+rm ..\MusicObjectDetector\protoc-3.4.0-win32.zip
 
 Stop-Transcript
