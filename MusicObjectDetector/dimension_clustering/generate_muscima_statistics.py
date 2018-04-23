8f927d34f72d69a56878db89f1e8a8f6f0133171
Alexander Pacha <alexander.pacha@gmail.com>
2018-04-23 15:29:02 +0200

    Updated dimension clustering for full-page clustering

--- MusicObjectDetector/dimension_clustering/generate_muscima_statistics.py
+++ MusicObjectDetector/dimension_clustering/generate_muscima_statistics.py
@@ -68,54 +68,12 @@ def create_statistics_for_full_images(glob_pattern_for_retrieving_muscima_images
     relative_statistics.to_csv(exported_relative_dimensions_file_path, float_format="%.5f", index=False)
 
 
-def create_statistics_for_cropped_images(annotations_csv,
-                                         path_to_cropped_images,
-                                         exported_absolute_dimensions_file_path,
-                                         exported_relative_dimensions_file_path):
-    annotations = pandas.read_csv(annotations_csv)
-    cropped_image_dimensions = []
-    for filename in tqdm(os.listdir(path_to_cropped_images)):
-        image = Image.open(os.path.join(path_to_cropped_images, filename))  # type: Image.Image
-        cropped_image_dimensions.append((filename, image.width, image.height))
-
-    width = (annotations['right'] - annotations['left']).rename('width')
-    height = (annotations['bottom'] - annotations['top']).rename('height')
-    x_center = (annotations['left'] + width / 2).rename('x_center')
-    y_center = (annotations['top'] + height / 2).rename('y_center')
-
-    absolute_annotations = pandas.concat(
-        [annotations[['filename', 'class', 'left', 'right', 'top', 'bottom']], x_center, y_center, width, height],
-        axis=1)  # type: pandas.DataFrame
-    absolute_annotations.to_csv(exported_absolute_dimensions_file_path, float_format="%.5f", index=False)
-
-    # Compute relative annotations
-    image_dimensions = pandas.DataFrame(cropped_image_dimensions, columns=['filename', 'image_width', 'image_height'])
-    joined = annotations.merge(image_dimensions, on='filename')
-    left = (joined['left'] / joined['image_width']).rename('left')
-    right = (joined['right'] / joined['image_width']).rename('right')
-    top = (joined['top'] / joined['image_height']).rename('top')
-    bottom = (joined['bottom'] / joined['image_height']).rename('bottom')
-    x_center = (x_center / joined['image_width']).rename('x_center')
-    y_center = (y_center / joined['image_height']).rename('y_center')
-    width = (width / joined['image_width']).rename('width')
-    height = (height / joined['image_height']).rename('height')
-
-    relative_annotations = pandas.concat(
-        [joined[['filename', 'class']], left, right, top, bottom, x_center, y_center, width, height],
-        axis=1)  # type: pandas.DataFrame
-    relative_annotations.to_csv(exported_relative_dimensions_file_path, float_format="%.5f", index=False)
-
-
 if __name__ == "__main__":
-    dataset_directory = "data"
+    dataset_directory = "../data"
     muscima_pp_raw_dataset_directory = os.path.join(dataset_directory, "muscima_pp_raw")
     muscima_image_directory = os.path.join(dataset_directory, "cvcmuscima_staff_removal")
 
-    create_statistics_for_full_images("data/cvcmuscima_staff_removal/*/ideal/*/image/*.png", "data/muscima_pp_raw",
-                                      "data/bounding_box_dimensions_absolute.csv",
-                                      "data/bounding_box_dimensions_relative.csv")
-
-    create_statistics_for_cropped_images(annotations_csv="data/Annotations.csv",
-                                         path_to_cropped_images="data/muscima_pp_cropped_images_with_stafflines",
-                                         exported_absolute_dimensions_file_path="data/bounding_box_dimensions_cropped_images_absolute.csv",
-                                         exported_relative_dimensions_file_path="data/bounding_box_dimensions_cropped_images_relative.csv")
+    create_statistics_for_full_images(os.path.join(muscima_image_directory, "*/ideal/*/image/*.png"),
+                                      muscima_pp_raw_dataset_directory,
+                                      "bounding_box_dimensions_absolute.csv",
+                                      "bounding_box_dimensions_relative.csv")
--- MusicObjectDetector/dimension_clustering/muscima_dimension_clustering.py
+++ MusicObjectDetector/dimension_clustering/muscima_dimension_clustering.py
@@ -1,3 +1,4 @@
+import os
 import random
 from glob import glob
 from typing import Tuple
@@ -6,8 +7,12 @@ import cv2
 import numpy
 import pandas
 import matplotlib.pyplot as plt
+import seaborn
 from PIL import Image
+from imblearn.over_sampling import SMOTE
+from pandas import DataFrame
 from tqdm import tqdm
+import pandas as pd
 
 
 def IOU(x, centroids):
@@ -84,9 +89,37 @@ def visualize_anchors(anchors: numpy.ndarray, visualization_width: int = 1000, v
         # print(w, h)
         left_upper_corner = (10 + i * stride_w, 10 + i * stride_h)
         right_lower_corner = (left_upper_corner[0] + w, left_upper_corner[1] + h)
-        cv2.rectangle(blank_image, left_upper_corner, right_lower_corner, colors[i])
+        cv2.rectangle(blank_image, left_upper_corner, right_lower_corner, colors[i], thickness=3)
 
     cv2.imwrite("anchors-{0}.png".format(len(anchors)), blank_image)
+
+
+def resample_dataset(dimensions: DataFrame, resampling_method='svm') -> DataFrame:
+    reproducible_seed = 42
+
+    report("Class distribution before resampling")
+    class_statistics = dimensions[['class']].groupby('class').size()
+    report(str(class_statistics))
+
+    report("Resampling with SMOTE ({0})".format(resampling_method))
+    # See http://contrib.scikit-learn.org/imbalanced-learn/stable/auto_examples/over-sampling/plot_comparison_over_sampling.html for a comparison between different resampling methods
+    smote = SMOTE(random_state=reproducible_seed, kind=resampling_method)
+    X_resampled, y_resampled = smote.fit_sample(dimensions[["width", "height"]],
+                                                dimensions["class"])
+    y = DataFrame(y_resampled)
+    y.columns = ['class']
+
+    report("Class distribution after resampling")
+    report(str(y.groupby('class').size()))
+    resampled_annotations = pd.concat([DataFrame(X_resampled), DataFrame(y_resampled)], axis=1)  # type: DataFrame
+    resampled_annotations.columns = ["width", "height", "class"]
+    return resampled_annotations
+
+
+def report(text):
+    with open("dimension_clustering_protocol.txt", "a") as dimension_clustering_protocol:
+        print(text)
+        dimension_clustering_protocol.writelines(text + "\n")
 
 
 if __name__ == "__main__":
@@ -96,11 +129,11 @@ if __name__ == "__main__":
     # visualization_width, visualization_height = 3500, 1500
 
     # Use this part for cropped images
-    annotation_dimensions = pandas.read_csv("data/bounding_box_dimensions_cropped_images_relative.csv")
-    all_cropped_images = glob("data/muscima_pp_cropped_images_with_stafflines/*.jpg")
+    annotation_dimensions = pandas.read_csv("bounding_box_dimensions_relative.csv")
+    all_images = glob("../data/cvcmuscima_staff_removal/CvcMuscima-Distortions/ideal/**/image/*.png", recursive=True)
     sizes = []
-    for cropped_image in tqdm(all_cropped_images, desc="Collecting image sizes"):
-        image = Image.open(cropped_image)
+    for image_path in tqdm(all_images, desc="Collecting image sizes"):
+        image = Image.open(image_path)
         sizes.append(image.size)
     sizes_df = pandas.DataFrame(sizes, columns=["width", "height"])
     visualization_width, visualization_height = sizes_df["width"].mean(), sizes_df["height"].mean()
@@ -123,17 +156,16 @@ if __name__ == "__main__":
         meanIntersectionOverUnion, centroids = kmeans(dims, initial_centroids)
         statistics.append((num_clusters, meanIntersectionOverUnion, centroids))
 
-    grid_size = 32
+    grid_size = 16
     for (clusters, iou, centroids) in statistics:
         print("{0} clusters: {1:.4f} mean IOU".format(clusters, iou))
         scales = []
         for c in centroids:
             print(
                 "[{0:.4f} {1:.4f}] - Ratio: {2:.4f} = {3:.0f}x{4:.0f}px scaled "
-                "to {5:.0f}x{6:.0f} image or {7:.2f}x{8:.2f}px relative to {9}x{9} grid".format(
+                "to {5:.0f}x{6:.0f} image".format(
                     c[0], c[1], c[0] / c[1], c[0] * visualization_width, c[1] * visualization_height,
-                    visualization_width, visualization_height, c[0] * grid_size, c[1] * grid_size,
-                    grid_size))
+                    visualization_width, visualization_height))
             scales.append(c[0] * visualization_width / grid_size)
             scales.append(c[1] * visualization_height / grid_size)
         scales.sort()
