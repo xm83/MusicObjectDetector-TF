d7c3d97fad0e8906e918d3de3a68776f42f59a33
Alexander Pacha <alexander.pacha@gmail.com>
2018-07-24 22:44:37 +0200

    Fixing required paths in preparation script

--- MusicObjectDetector/PrepareDatasetsForTensorflow.ps1
+++ MusicObjectDetector/PrepareDatasetsForTensorflow.ps1
@@ -5,8 +5,8 @@ $pathToTranscript = "$($pathToSourceRoot)"
 Start-Transcript -path "$($pathToTranscript)Transcript.txt" -append
 
 cd $pathToSourceRoot
-echo "Appending research folder $($pathToGitRoot)research to temporary PYTHONPATH"
-$env:PYTHONPATH = $env:PYTHONPATH;"$($pathToSourceRoot)research"
+echo "Appending required paths to temporary PYTHONPATH"
+$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot)/research/slim;$($pathToSourceRoot)"
 
 echo "Testing correct setup"
 cd ../research
@@ -15,9 +15,9 @@ python object_detection/builders/model_builder_test.py
 echo "Generating data-record in Tensorflow-format"
 cd ../MusicObjectDetector
 
-python download_muscima_dataset.py
-python prepare_muscima_annotations.py
-python dataset_splitter.py --source_directory=data/muscima_pp_images --destination_directory=data/training_validation_test
+# python download_muscima_dataset.py
+# python prepare_muscima_annotations.py
+# python dataset_splitter.py --source_directory=data/muscima_pp_images --destination_directory=data/training_validation_test
 
 python create_muscima_tf_record.py --data_dir=data/training_validation_test --set=training --annotations_dir=Annotations --output_path=data/all_classes_writer_independent_split/training.record --label_map_path=mapping_all_classes.txt
 python create_muscima_tf_record.py --data_dir=data/training_validation_test --set=validation --annotations_dir=Annotations --output_path=data/all_classes_writer_independent_split/validation.record --label_map_path=mapping_all_classes.txt
