ef74da671bbc81f75ad14f0ec106e683fd32faf1
Alexander Pacha <alexander.pacha@gmail.com>
2018-07-25 13:52:04 +0200

    Completed the creation of the tf-record from the normalized dataset

--- MusicObjectDetector/PrepareNormalizedDatasetsForTensorflow.ps1
+++ MusicObjectDetector/PrepareNormalizedDatasetsForTensorflow.ps1
@@ -18,12 +18,22 @@ cd ../MusicObjectDetector
 # 1. Manually copy the normalized directory into the data folder
 
 # 2. Create the mappings
-# python generate_mapping_for_normalized_dataset.py --normalized_dataset_dir ./data/normalized/muscima --mapping_output_path ./data/normalized/muscima/mapping.txt
-# python generate_mapping_for_normalized_dataset.py --normalized_dataset_dir ./data/normalized/deepscores --mapping_output_path ./data/normalized/deepscores/mapping.txt
-# python generate_mapping_for_normalized_dataset.py --normalized_dataset_dir ./data/normalized/mensural --mapping_output_path ./data/normalized/mensural/mapping.txt
+python generate_mapping_for_normalized_dataset.py --normalized_dataset_dir ./data/normalized/deepscores --mapping_output_path ./data/normalized/deepscores/mapping.txt
+python generate_mapping_for_normalized_dataset.py --normalized_dataset_dir ./data/normalized/mensural --mapping_output_path ./data/normalized/mensural/mapping.txt
+python generate_mapping_for_normalized_dataset.py --normalized_dataset_dir ./data/normalized/muscima --mapping_output_path ./data/normalized/muscima/mapping.txt
 
 # 3. Create the records
 python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/deepscores --set=training --output_path=data/normalized/deepscores/training.record --label_map_path=data/normalized/deepscores/mapping.txt
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/deepscores --set=validation --output_path=data/normalized/deepscores/validation.record --label_map_path=data/normalized/deepscores/mapping.txt
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/deepscores --set=test --output_path=data/normalized/deepscores/test.record --label_map_path=data/normalized/deepscores/mapping.txt
+
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/mensural --set=training --output_path=data/normalized/mensural/training.record --label_map_path=data/normalized/mensural/mapping.txt
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/mensural --set=validation --output_path=data/normalized/mensural/validation.record --label_map_path=data/normalized/mensural/mapping.txt
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/mensural --set=test --output_path=data/normalized/mensural/test.record --label_map_path=data/normalized/mensural/mapping.txt
+
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/muscima --set=training --output_path=data/normalized/muscima/training.record --label_map_path=data/normalized/muscima/mapping.txt
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/muscima --set=validation --output_path=data/normalized/muscima/validation.record --label_map_path=data/normalized/muscima/mapping.txt
+python create_tf_record_from_normalized_dataset.py --data_dir=data/normalized/muscima --set=test --output_path=data/normalized/muscima/test.record --label_map_path=data/normalized/muscima/mapping.txt
 
 Stop-Transcript
 
--- MusicObjectDetector/create_tf_record_from_normalized_dataset.py
+++ MusicObjectDetector/create_tf_record_from_normalized_dataset.py
@@ -4,6 +4,7 @@ import os
 
 import PIL.Image
 import tensorflow as tf
+from typing import List, Dict
 
 from object_detection.utils import dataset_util
 from object_detection.utils import label_map_util
@@ -24,9 +25,9 @@ FLAGS = flags.FLAGS
 SETS = ['training', 'validation', 'test']
 
 
-def csv_row_to_tf_example(example,
-                          dataset_directory,
-                          label_map_dict):
+def annotations_to_tf_example_list(annotations: pd.DataFrame,
+                                   dataset_directory: str,
+                                   label_map_dict: Dict[str, int]) -> List[tf.train.Example]:
     """Convert XML derived dict to tf.Example proto.
 
     Notice that this function normalizes the bounding box coordinates provided
@@ -43,50 +44,67 @@ def csv_row_to_tf_example(example,
     Raises:
       ValueError: if the image pointed to by data['filename'] is not a valid JPEG
     """
-    img_path = example['path_to_image']
-    full_path = os.path.join(dataset_directory, img_path)
-    with tf.gfile.GFile(full_path, 'rb') as fid:
-        encoded_png = fid.read()
-    encoded_png_io = io.BytesIO(encoded_png)
-    image = PIL.Image.open(encoded_png_io)
-    if image.format != 'PNG':
-        raise ValueError('Image format not PNG')
-    key = hashlib.sha256(encoded_png).hexdigest()
 
-    width = image.width
-    height = image.height
+    examples = []
+    total_number_of_images = len(annotations["path_to_image"].unique())
+    for path_to_image, image_annotations in tqdm(annotations.groupby("path_to_image"),
+                                                 desc="Serializing annotations",
+                                                 total=total_number_of_images):
 
-    xmin = [float(example['left']) / width]
-    ymin = [float(example['top']) / height]
-    xmax = [float(example['right'])/ width]
-    ymax = [float(example['bottom'])/ height]
-    classes = [label_map_dict[example['class_name']]]
-    classes_text = [example['class_name'].encode('utf8')]
-    truncated = []
-    poses = []
-    difficult_obj = []
+        full_path = os.path.join(dataset_directory, path_to_image)
+        with tf.gfile.GFile(full_path, 'rb') as fid:
+            encoded_png = fid.read()
+        encoded_png_io = io.BytesIO(encoded_png)
+        image = PIL.Image.open(encoded_png_io)
+        if image.format != 'PNG':
+            raise ValueError('Image format not PNG')
+        key = hashlib.sha256(encoded_png).hexdigest()
 
-    example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(
-            img_path.encode('utf8')),
-        'image/source_id': dataset_util.bytes_feature(
-            img_path.encode('utf8')),
-        'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
-        'image/encoded': dataset_util.bytes_feature(encoded_png),
-        'image/format': dataset_util.bytes_feature('png'.encode('utf8')),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-        'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
-        'image/object/truncated': dataset_util.int64_list_feature(truncated),
-        'image/object/view': dataset_util.bytes_list_feature(poses),
-    }))
-    return example
+        width = image.width
+        height = image.height
+
+        xmin = []
+        ymin = []
+        xmax = []
+        ymax = []
+        classes = []
+        classes_text = []
+        truncated = []
+        poses = []
+        difficult_obj = []
+
+        for index, example in image_annotations.iterrows():
+            xmin.append(float(example['left']) / width)
+            ymin.append(float(example['top']) / height)
+            xmax.append(float(example['right']) / width)
+            ymax.append(float(example['bottom']) / height)
+            classes.append(label_map_dict[example['class_name']])
+            classes_text.append(example['class_name'].encode('utf8'))
+
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'image/height': dataset_util.int64_feature(height),
+            'image/width': dataset_util.int64_feature(width),
+            'image/filename': dataset_util.bytes_feature(
+                path_to_image.encode('utf8')),
+            'image/source_id': dataset_util.bytes_feature(
+                path_to_image.encode('utf8')),
+            'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+            'image/encoded': dataset_util.bytes_feature(encoded_png),
+            'image/format': dataset_util.bytes_feature('png'.encode('utf8')),
+            'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
+            'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
+            'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
+            'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
+            'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+            'image/object/class/label': dataset_util.int64_list_feature(classes),
+            'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+            'image/object/truncated': dataset_util.int64_list_feature(truncated),
+            'image/object/view': dataset_util.bytes_list_feature(poses),
+        }))
+
+        examples.append(example)
+
+    return examples
 
 
 def main(_):
@@ -105,11 +123,10 @@ def main(_):
     annotations_path = os.path.join(data_dir, dataset_split + '.csv')
 
     annotations = pd.read_csv(annotations_path)
-    for idx, example in tqdm(annotations.iterrows(),
-                             desc="Parsing annotations from {0} set into TF-Example".format(dataset_split),
-                             total=len(annotations)):
+    tf_examples = annotations_to_tf_example_list(annotations, data_dir, label_map_dict)
 
-        tf_example = csv_row_to_tf_example(example, data_dir, label_map_dict)
+    for tf_example in tqdm(tf_examples,
+                           desc="Serializing annotations from {0} set into TF-Example".format(dataset_split)):
         writer.write(tf_example.SerializeToString())
 
     writer.close()
