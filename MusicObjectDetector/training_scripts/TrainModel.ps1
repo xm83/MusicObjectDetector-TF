0236a3ba7a1e80f242c96b56dd73190b8f655ebc
Alexander Pacha <alexander.pacha@gmail.com>
2018-08-07 12:32:08 +0200

    Updating TrainModel.ps1 to support both legacy and estimator based training

--- MusicObjectDetector/training_scripts/TrainModel.ps1
+++ MusicObjectDetector/training_scripts/TrainModel.ps1
@@ -8,10 +8,7 @@ cd $pathToGitRoot
 #echo "Appending required paths to temporary PYTHONPATH"
 #$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToSourceRoot)"
 
-python research/object_detection/model_main.py --pipeline_config_path="C:/Users/Alex/Repositories/MusicObjectDetector-TF2/research/object_detection/samples/configs/ssd_mobilenet_v1_0.75_depth_quantized_300x300_pets_sync.config" --model_dir="$($pathToData)/pet-train" --alsologtostderr
 
-
-exit
 
 ################################################################
 # Available configurations - uncomment the one to actually run #
@@ -22,10 +19,17 @@ exit
 #$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_deepscores_1"
 #$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_mensural_1"
 #$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_muscima_1"
-$configuration = "faster_rcnn_inc_resnet_v2_muscima_1"
-$configuration = "faster_rcnn_resnet50_muscima"
+#$configuration = "faster_rcnn_inc_resnet_v2_muscima_1"
+$configuration = "ssd_resnet50_retinanet_muscima_1"
 
-Start-Transcript -path "$($pathToTranscript)/TrainModel-$($configuration).txt" -append
 echo "Training with $($configuration) configuration"
-python research/object_detection/model_main.py --alsologtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --model_dir="$($pathToData)/checkpoints-$($configuration)"
+
+# Legacy slim-based
+Start-Transcript -path "$($pathToTranscript)/Train-$($configuration).txt" -append
+python research/object_detection/legacy/train.py --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --train_dir="$($pathToData)/checkpoints-$($configuration)-train"
 Stop-Transcript
+
+# # Estimator-based
+# Start-Transcript -path "$($pathToTranscript)/TrainEval-$($configuration).txt" -append
+# python research/object_detection/model_main.py --alsologtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --model_dir="$($pathToData)/$($configuration)"
+# Stop-Transcript
