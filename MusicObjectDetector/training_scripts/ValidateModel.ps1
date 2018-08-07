af87d37270cf4f78b7925a06e9f0aca272858866
Alexander Pacha <alexander.pacha@gmail.com>
2018-08-07 14:32:08 +0200

    Updating ValidateModel.ps1 to reflect new way of training

--- MusicObjectDetector/training_scripts/ValidateModel.ps1
+++ MusicObjectDetector/training_scripts/ValidateModel.ps1
@@ -5,8 +5,8 @@ $pathToData = "$($pathToSourceRoot)/data"
 #$pathToData = "\\MONSTI\MusicObjectDetector-TF_Results"
 cd $pathToGitRoot
 
-echo "Appending required paths to temporary PYTHONPATH"
-$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot)/research/slim;$($pathToSourceRoot)"
+#echo "Appending required paths to temporary PYTHONPATH"
+#$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot)/research/slim;$($pathToSourceRoot)"
 
 ################################################################
 # Available configurations - uncomment the one to actually run #
@@ -16,10 +16,14 @@ $env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot
 #$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_dimension_clustering_rms_2000_proposals"
 #$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_deepscores_1"
 #$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_mensural_1"
-$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_muscima_1"
+#$configuration = "faster_rcnn_inception_resnet_v2_atrous_pretrained_muscima_1"
+#$configuration = "faster_rcnn_inc_resnet_v2_muscima_1"
+$configuration = "ssd_resnet50_retinanet_muscima_1"
 
-Start-Transcript -path "$($pathToTranscript)/ValidateModel-$($configuration).txt" -append
 echo "Validate with $($configuration) configuration"
-python research/object_detection/eval.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --checkpoint_dir="$($pathToData)/checkpoints-$($configuration)-train" --eval_dir="$($pathToData)/checkpoints-$($configuration)-validate"
+
+# Legacy slim-based evaluation
+Start-Transcript -path "$($pathToTranscript)/Validate-$($configuration).txt" -append
+python research/object_detection/legacy/eval.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --checkpoint_dir="$($pathToData)/checkpoints-$($configuration)-train" --eval_dir="$($pathToData)/checkpoints-$($configuration)-validate"
 Stop-Transcript
 
